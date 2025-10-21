f193ed229b0422270b31cdb8b553a49f52305173
azrilpramudia <azrilpramudia01@gmail.com>
2025-10-21 13:15:32 +0700

    feat: add hero

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section>
+      <div className="flex justify-center">
+        <h1 className="text-2xl font-bold py-5">Hero</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
