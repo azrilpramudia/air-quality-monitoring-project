190cd3ed55ae44581d33c1c1dd496335a56e9632
azrilpramudia <azrilpramudia01@gmail.com>
2025-10-21 11:20:49 +0700

    feat: add inital setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="bg-blue-600 py-5">
+        <h1 className="text-2xl text-white font-bold flex justify-center">
+          Navbar
+        </h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
